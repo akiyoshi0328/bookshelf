cad0825f2530cfea3335169bbab8070e409f0869
akiyoshi0328 <akiyoshi.takahashi.50@gmail.com>
2019-12-09 13:49:53 +0900

    データ修正いたしました

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,7 @@ class Book < ApplicationRecord
   has_one_attached :image
   attribute :new_image
 
-  validates :title, presence: true, length: {maximum: 50 }
+  validates :title, presence: true, length: { maximum: 50 }
   validates :price, presence: true, numericality: { only_integer: true,greater_than_or_equal_to: 1 }
   validates :publish_date, presence: true
   validates :description, presence: true, length:{ maximum: 1000 }
